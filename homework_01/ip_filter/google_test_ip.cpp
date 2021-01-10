fb37e415321cc1fe13714094b67e7b2a117df579
prickly-u <kterehova@gmail.com>
2021-01-10 18:55:11 +0300

    Not failing test

--- homework_01/ip_filter/google_test_ip.cpp
+++ homework_01/ip_filter/google_test_ip.cpp
@@ -1,11 +1,12 @@
 #include <gtest/gtest.h>
 
-TEST(TestInput, Split)
+TEST(CheckGTestInstalled, AlwaysTrue)
 {
-    ASSERT_GT(0, 1);
+    ASSERT_GT(2, 1);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
