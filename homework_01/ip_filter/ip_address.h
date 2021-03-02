665b147c9fcda4cae0bd6375da3a3f255040283b
prickly-u <kterehova@gmail.com>
2021-03-02 14:32:29 +0300

    Review corrections

--- homework_01/ip_filter/ip_address.h
+++ homework_01/ip_filter/ip_address.h
@@ -5,12 +5,20 @@
 #include <algorithm>
 #include <array>
 #include <iostream>
+#include <functional>
 #include <stdexcept>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+
+/*!
+ * Generalized IP address.
+ * @tparam  T  The type to store parts of IP.
+ * @tparam  L  The amout of parts in the IP address. 
+ * @tparam  S  The separator used in parsing and output of IP address.
+ */
 template <typename T, ushort L, char S>
 class IpAddress
 {
@@ -30,7 +38,8 @@ class IpAddress
 
     private:
         array<T, L> m_bytes;
-        array<T, L> parse_ip(const string &str);
+        template<typename SourceEnumerable, typename Part>
+        void InitializeFrom(const SourceEnumerable& source, function<T(Part)> parse);
 
 };
 
@@ -39,86 +48,49 @@ IpAddress<T, L, S>::IpAddress(const string &str)
 {
     auto ip_strs = split(str, S);
 
-    if (ip_strs.size() != L)
-    {
-        auto what = "Incorrect number of parts in ip string '" + str + "'. Expected: " + to_string(L); 
-        throw invalid_argument(what);
-    }
-
-    int i = 0;
-    for (auto const &part: ip_strs)
-    {
-        try
-        {
-            m_bytes[i++] = stoi(part);
-        }
-        catch(const exception &e)
-        {
-            auto what = "Invalid input string with IP: '" + str + "'. Details: " + e.what();
-            throw invalid_argument(what);
-        }
-    }
+    InitializeFrom<vector<string>, string>(ip_strs, [](string str){ return stoi(str);});
 }
 
 template <typename T, ushort L, char S>
 IpAddress<T, L, S>::IpAddress(const initializer_list<int> &bytes)
 {
-    int i = 0;
-    for (auto const &byte: bytes)
-    {
-        m_bytes[i++] = (uint8_t)byte;
-    }
+    InitializeFrom<initializer_list<int>, int>(bytes, [](int byte){ return (T)byte;});
 }
 
 template <typename T, ushort L, char S>
 bool IpAddress<T, L, S>::operator==(const IpAddress &other ) const 
 {
-     return this->m_bytes == other.m_bytes; 
+     return m_bytes == other.m_bytes; 
 }
 
 template <typename T, ushort L, char S>
 bool IpAddress<T, L, S>::operator>=(const IpAddress &other ) const 
 {
-     return this->m_bytes >= other.m_bytes; 
+     return m_bytes >= other.m_bytes; 
 }
 
 template <typename T, ushort L, char S>
 bool IpAddress<T, L, S>::operator<=(const IpAddress &other ) const 
 {
-     return this->m_bytes <= other.m_bytes; 
+     return m_bytes <= other.m_bytes; 
 }
 
 template <typename T, ushort L, char S>
 bool IpAddress<T, L, S>::operator>(const IpAddress &other ) const 
 {
-     return this->m_bytes > other.m_bytes; 
+     return m_bytes > other.m_bytes; 
 }
 
 template <typename T, ushort L, char S>
 bool IpAddress<T, L, S>::operator<(const IpAddress &other ) const 
 {
-     return this->m_bytes < other.m_bytes; 
+     return m_bytes < other.m_bytes; 
 }
 
 template <typename T, ushort L, char S>
 bool IpAddress<T, L, S>::operator!=(const IpAddress &other ) const 
 {
-     return this->m_bytes != other.m_bytes; 
-}
-
-template <typename T, ushort L, char S>
-array<T, L> IpAddress<T, L, S>::parse_ip(const string &str)
-{
-    auto ip_strs = split(str, '.');
-    array<T, L> bytes;
-
-    int i = 0;
-    for (auto const &part: ip_strs)
-    {
-        bytes[i] = stoi(part);
-    }
-
-    return bytes;
+     return m_bytes != other.m_bytes; 
 }
 
 template <typename T, ushort L, char S>
@@ -136,12 +108,37 @@ bool IpAddress<T, L, S>::ContainsByte(T searched_byte) const
 template <typename T, ushort L, char S>
 void IpAddress<T, L, S>::output_ip(ostream& os) const
 {
-    auto separator = "";
-    for_each(this->m_bytes.cbegin(), this->m_bytes.cend(), [&os, &separator](uint8_t part)
+    auto separator = '\0';
+    for_each(m_bytes.cbegin(), m_bytes.cend(), [&os, &separator](uint8_t part)
     {
         os << separator << (int)part;
-        separator = ".";
+        separator = S;
     });
 
     os << endl;
+}
+
+template <typename T, ushort L, char S>
+template<typename SourceEnumerable, typename Part>
+void IpAddress<T, L, S>::InitializeFrom(const SourceEnumerable& source, function<T(Part)> parse)
+{
+    if (source.size() != L)
+    {
+        auto what = "Incorrect number of parts in ip string. Expected: " + to_string(L); 
+        throw invalid_argument(what);
+    }
+
+    int i = 0;
+    for (auto const &part: source)
+    {
+        try
+        {
+            m_bytes[i++] = parse(part);
+        }
+        catch(const exception &e)
+        {
+            auto what = string("Invalid IP address. Details: ") + e.what();
+            throw invalid_argument(what);
+        }
+    }
 }
