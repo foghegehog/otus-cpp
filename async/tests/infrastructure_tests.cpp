bedbd4f72a4cb00561f786674d7fbbd032c14184
prickly-u <kterehova@gmail.com>
2021-05-04 18:24:36 +0300

    Tests for bulk writer

--- async/tests/infrastructure_tests.cpp
+++ async/tests/infrastructure_tests.cpp
@@ -1,9 +1,11 @@
 #include "../handlers_factory.h"
 #include "../handlers/accumulator.h"
 #include "../handlers/control_unit.h"
+#include "../postprocessing/bulk_writer.h"
 
 #include <gtest/gtest.h>
 #include <memory>
+#include <sstream>
 #include <vector>
 
 TEST(Infrastructure, HandlersChain)
@@ -23,4 +25,18 @@ TEST(Infrastructure, HandlersChain)
 
     ASSERT_EQ(accumulator->GetCommandsStoredCount(), commands.size());
     ASSERT_TRUE(control_unit->ShouldClearProcessedBulk());
+}
+
+TEST(Infrastructure, OutputFormatting)
+{
+    using namespace handlers;
+    using namespace std;
+
+    vector<ExecutableCommand> commands = { ExecutableCommand("cmd1"), ExecutableCommand("cmd2"), ExecutableCommand("cmd3") };  
+    stringstream outstream;
+    postprocessing::BulkWriter writer(outstream);
+    writer.WriteBulk(commands);
+
+    ASSERT_EQ(outstream.str(), "bulk: cmd1, cmd2, cmd3");
+
 }
