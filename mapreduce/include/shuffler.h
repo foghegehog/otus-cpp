aee3bab03f5c2db1ec7810ad96451873cd9f0ce2
prickly-u <kterehova@gmail.com>
2021-06-09 23:09:12 +0300

    Comments on shuffle algo

--- mapreduce/include/shuffler.h
+++ mapreduce/include/shuffler.h
@@ -35,6 +35,7 @@ void shuffler<K, V>::run(
         per_reducer_count += 1;
     }
 
+    // Traces current non-copied position of each non-traversed container
     std::list<container_trace<std::vector<std::pair<K, V>>>> containers;
     for(auto& container: after_map)
     {
@@ -46,8 +47,11 @@ void shuffler<K, V>::run(
 
     auto reducer = for_reduce.begin();
     reducer->reserve(per_reducer_count);
+
+    // Extrapolate merge step of the merge sort algorithm to the case of multiple containers instead of 2
     while(!containers.empty())
     {
+        // Find the first container with current copying position having key less then of all others containers' pos
         auto min_key_container = containers.begin();
         auto min_key = min_key_container->m_pos->first;
 
@@ -60,13 +64,16 @@ void shuffler<K, V>::run(
             }
         }
 
+        // For all containers with position containing min key copy min key elements to target
         for(auto container_it = min_key_container; container_it != containers.end(); ++container_it)
         {
+            // Skip if not the case
             if (container_it->m_pos->first != min_key)
             {
                 continue;
             }
 
+            // Get iterators for range of positions with min key
             auto from_iterator = std::make_move_iterator(container_it->m_pos);
             auto end_iterator = std::make_move_iterator(container_it->m_end);
             auto to_iterator = std::make_move_iterator(container_it->m_pos);
@@ -77,16 +84,23 @@ void shuffler<K, V>::run(
                 ++count;
             }
 
+            // Copy all min key elements of the current container to target
             reducer->insert(reducer->end(), from_iterator, to_iterator);
+
+            // Advance current copying position for amount of copyed elements
             std::advance(container_it->m_pos, count);
+
+            // If all elements of the container were copied, remove the container from the list
             if (container_it->is_finished())
             {
                 auto to_remove = container_it;
-                --container_it;
+                // Will be increased in the loop iteration to the element next to removed 
+                --container_it; 
                 containers.erase(to_remove);
             }
         }
 
+        // If target reducer is full, move to next one
         if (reducer->size() >= per_reducer_count)
         {
             ++reducer;
